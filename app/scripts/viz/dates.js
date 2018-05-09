6627a023c5b5e0195d79b05c1e7d0e8e902c63e3
Zach Kirsch <zachkirsch@gmail.com>
2018-05-09 00:21:49 -0400

    snazzy dates

--- app/scripts/viz/dates.js
+++ app/scripts/viz/dates.js
@@ -1,37 +1,13 @@
-function changeDatesScatter(airport_code) {
-  let svg = d3.select("#dates-svg")
-  let xMax = d3.max(claims, d => d.incidentDate)
-  let xMin = d3.min(claims, d => d.incidentDate)
-  let yMax = d3.max(claims, d => d.closeAmount)
-  let yMin = d3.min(claims, d => d.closeAmount)
-  let [scaleLength, majorMax] = xMax > yMax ? [width, xMax] : [height, yMax]
-
-  let xScale = d3.scaleTime()
-      .domain([xMin, xMax])
-      .range([0, width])
-
-  let yScale = d3.scaleLinear()
-      .domain([yMin, yMax])
-      .range([height, 0])
-
-
-  svg.selectAll("circle")
-      .remove()
-      .data(claims)
-      .enter().append("circle")
-          .attr("cx", d => xScale(d.incidentDate))
-          .attr("cy", d => yScale(d.closeAmount))
-          .attr("r", 3.4)
-          .style("fill", "transparent")
-          .style("stroke", "rgba(108, 34, 125, 0.7)")
-          .style("stroke-width", 1.8)
-          .on("mouseover", function(d) {
-              console.log(d)
-          })
-}
-
-function setupDates() {
+let initialRender = true
+function setupDates(airport_code) {
     let svg = d3.select("#dates-svg")
+    if (airport_code === undefined && !initialRender) {
+      svg.selectAll("circle")
+        .transition()
+        .duration(500)
+        .style("opacity", 0)
+      return
+    }
     let width = 0.8 * (+svg.attr("width"))
     let height = 0.8 * (+svg.attr("height"))
 
@@ -43,14 +19,16 @@ function setupDates() {
     }
 
     // create a clipping region
-    svg.append("defs").append("clipPath")
-        .attr("id", "clip")
-      .append("rect")
-        .attr("width", width)
-        .attr("height", height)
+    if (initialRender) {
+      svg.append("defs").append("clipPath")
+          .attr("id", "clip")
+          .append("rect")
+          .attr("width", width)
+          .attr("height", height)
+    }
 
     d3.queue()
-      .defer(d3.tsv, "./data/viz/by-airport/BOS.tsv")
+      .defer(d3.tsv, "./data/viz/by-airport/" + (airport_code || 'PQI') + ".tsv")
       .await(ready)
 
     function ready(error, claims) {
@@ -71,9 +49,15 @@ function setupDates() {
             }))
 
         let xMax = d3.max(claims, d => d.incidentDate)
+        xMax = new Date(xMax.getTime() + 50000000000)
+
         let xMin = d3.min(claims, d => d.incidentDate)
-        let yMax = d3.max(claims, d => d.closeAmount)
+        xMin = new Date(xMin.getTime() - 50000000000)
+
+        let yMax = d3.max(claims, d => d.closeAmount) * 1.1 + 10
         let yMin = d3.min(claims, d => d.closeAmount)
+        yMin = yMin - (yMax - yMin) * .1 - 10
+
         let [scaleLength, majorMax] = xMax > yMax ? [width, xMax] : [height, yMax]
 
         let xScale = d3.scaleTime()
@@ -119,67 +103,100 @@ function setupDates() {
             .ticks(5, "s")
             .tickSize(-width);
 
-        let gX = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + (margin.top + height) + ")")
-            .call(xAxis)
+        let gX, gY
 
-        let gY = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .call(yAxis)
 
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0)
-            .attr("x", 0 - (height / 2))
-            .attr("dy", "10px")
-            .attr("class", "scatter-label")
-            .style("text-anchor", "middle")
-            .text("Close Amount")
+        if (initialRender) {
+          gX = svg.append("g")
+              .attr("transform", "translate(" + margin.left + "," + (margin.top + height) + ")")
+              .attr("class", "x")
+              .call(xAxis)
 
-        svg.append("text")
-            .attr("transform", "translate(" + (width / 2 + 50) + ", " + (height + margin.top + 30) + ")")
-            .style("text-anchor", "middle")
-            .text("Incident Date")
-
-        let circlesGroup = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .attr("clip-path", "url(#clip)")
-            .classed("circles-group", true)
-
-        let points = circlesGroup.selectAll("circle")
-            .data(claims)
-            .enter().append("circle")
-                .attr("cx", d => xScale(d.incidentDate))
-                .attr("cy", d => yScale(d.closeAmount))
-                .attr("r", 3.4)
-                .style("fill", "transparent")
-                .style("stroke", "rgba(108, 34, 125, 0.7)")
-                .style("stroke-width", 1.8)
-                .on("mouseover", function(d) {
-                    console.log(d)
-                })
-
-        let zoom = d3.zoom()
-            // .scaleExtent([.5, 20])
-            .extent([[0, 0], [width, height]])
-            .on("zoom", zoomed)
-
-        svg.append("rect")
-            .attr("width", width)
-            .attr("height", height)
-            .style("fill", "none")
-            .style("pointer-events", "all")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .call(zoom)
-
-        function zoomed() {
-            let newXScale = d3.event.transform.rescaleX(xScale)
-            let newYScale = d3.event.transform.rescaleY(yScale)
-            gX.call(xAxis.scale(newXScale))
-            gY.call(yAxis.scale(newYScale))
-            points.data(claims)
-                .attr("cx", d => newXScale(d.incidentDate))
-                .attr("cy", d => newYScale(d.closeAmount))
+          gY = svg.append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+              .attr("class", "y")
+              .call(yAxis)
+        } else {
+          gX = svg.select(".x")
+            gX.transition()
+             .duration(500)
+             .call(xAxis)
+          gY = svg.select(".y")
+            gY.transition(t)
+             .duration(500)
+             .call(yAxis)
         }
+
+        if (initialRender) {
+          svg.append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 0)
+              .attr("x", 0 - (height / 2))
+              .attr("dy", "10px")
+              .attr("class", "scatter-label")
+              .style("text-anchor", "middle")
+              .text("Close Amount")
+
+          svg.append("text")
+              .attr("transform", "translate(" + (width / 2 + 50) + ", " + (height + margin.top + 30) + ")")
+              .style("text-anchor", "middle")
+              .text("Incident Date")
+          }
+
+        let circlesGroup
+
+        if (initialRender) {
+          let circlesGroup = svg.append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+              .attr("clip-path", "url(#clip)")
+              .classed("circles-group", true)
+
+          for (let i = 0; i < 6148; i++) {
+            circlesGroup.append('circle')
+              .attr("cx", 0)
+              .attr("cy", 0)
+              .attr("r", 3.4)
+              .style("fill", "transparent")
+              .style("stroke", "rgba(108, 34, 125, 0.7)")
+              .style("opacity", 0)
+              .style("stroke-width", 1.8)
+          }
+        } else {
+          circlesGroup = svg.select(".circles-group")
+          let points = circlesGroup.selectAll("circle")
+                  .transition()
+                  .duration(1000)
+                  .attr("cx", (_, i) => claims[i] && xScale(claims[i].incidentDate))
+                  .attr("cy", (_, i) => claims[i] && yScale(claims[i].closeAmount))
+                  .attr("r", 3.4)
+                  .style("fill", "transparent")
+                  .style("stroke", "rgba(108, 34, 125, 0.7)")
+                  .style("stroke-width", 1.8)
+                  .style('opacity', (_, i) => claims[i] ? 1 : 0)
+
+          let zoom = d3.zoom()
+              // .scaleExtent([.5, 20])
+              .extent([[0, 0], [width, height]])
+              .on("zoom", zoomed)
+
+          svg.append("rect")
+              .attr("width", width)
+              .attr("height", height)
+              .style("fill", "none")
+              .style("pointer-events", "all")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+              .call(zoom)
+
+          function zoomed() {
+              let newXScale = d3.event.transform.rescaleX(xScale)
+              let newYScale = d3.event.transform.rescaleY(yScale)
+              gX.call(xAxis.scale(newXScale))
+              gY.call(yAxis.scale(newYScale))
+              circlesGroup.selectAll("circle")
+                  .attr("cx", (_, i) => claims[i] && newXScale(claims[i].incidentDate))
+                  .attr("cy", (_, i) => claims[i] && newYScale(claims[i].closeAmount))
+          }
+        }
+      initialRender = false
     }
 }
